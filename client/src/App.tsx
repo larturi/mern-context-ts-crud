91a5da2ab843da4d34d4ec0ce659c6e7528805fa
Leandro Arturi <lea.arturi@gmail.com>
2022-09-28 18:11:08 -0300

    Tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,11 +5,15 @@ import './App.css';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <HomePage /> } />
-      <Route path="/new" element={ <PostFormPage /> } />
-      <Route path="*" element={ <NotFoundPage /> } />
-    </Routes>
+    <div className='bg-neutral-900 min-h-screen flex items-center'>
+      <div className='px-10 container m-auto'>
+        <Routes>
+          <Route path="/" element={ <HomePage /> } />
+          <Route path="/new" element={ <PostFormPage /> } />
+          <Route path="*" element={ <NotFoundPage /> } />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
